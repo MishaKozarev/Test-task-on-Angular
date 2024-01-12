f7a3f14890900c2530de5cb187d12006149b1734
MishaKozarev <mishaaaaa1986@mail.ru>
2024-01-12 22:54:13 +0300

    feat: add tooltip component

--- test-task/src/app/core/core.module.ts
+++ test-task/src/app/core/core.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { TooltipComponent } from './components/tooltip/tooltip.component';
 
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TooltipComponent
+  ],
   imports: [
     CommonModule,
     HttpClientModule
+  ],
+  exports: [
+    TooltipComponent
   ]
 })
 export class CoreModule { }
