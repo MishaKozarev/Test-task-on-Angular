0d933e14f67ce41e9831357d9edad342c33e1fdd
MishaKozarev <mishaaaaa1986@mail.ru>
2024-01-23 14:44:05 +0300

    feat: change start page in routing

--- test-task/src/app/app-routing.module.ts
+++ test-task/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    // redirectTo: 'login',
+    redirectTo: 'dashboard',
     pathMatch: 'full'
   },
   {
